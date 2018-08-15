93f0e7ff226b02b2b153f570b8b5dfb973f29263
PK <pradhyu.shrestha@gmail.com>
2018-08-14 23:48:18 -0400

    Initial commit with initial work on julia practise

---
+++ hello-julia.jl
@@ -0,0 +1,20 @@
+
+# simple hello world 
+println("Hello")
+
+
+# function definition 
+# Bummer doesn't take - in function like in lisp 
+function square_x
+    me(num)
+    return num*num
+end
+
+square_me(100)
+
+# function can be also definied more succintly 
+
+
+
+
+
